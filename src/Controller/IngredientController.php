1c6ef950af62a752f0a6621285b0eea80fe6a17e
klhostis2022 <k.lhostis1@gmail.com>
2023-04-20 14:20:29 +0200

    Fixtures

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class IngredientController extends AbstractController
 {
-    #[Route('/', name: 'home_index', methods:['GET'])]
+    #[Route('/ingredient', name: 'app_ingredient')]
     public function index(): Response
     {
-        return $this->render('pages/home.html.twig');
+        return $this->render('pages/ingredient/index.html.twig', [
+            'controller_name' => 'IngredientController',
+        ]);
     }
 }
